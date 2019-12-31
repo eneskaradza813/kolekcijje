ad6d43f49958806a3785e58e4f847421591626f4
enes <mesa@DESKTOP-6T2EFT8>
2019-12-31 20:24:58 +0100

    23456789  kolekcije
    = vjezba kolekcije

---
+++ src/vjezba/pkg17/Node.java
@@ -0,0 +1,13 @@
+package vjezba.pkg17;
+
+public class Node {
+
+    
+    public Node(int value){
+        this.value = value;
+    }
+    
+    public int value;
+    public Node nextNode;
+    
+}
---
+++ src/vjezba/pkg17/Vjezba17.java
@@ -0,0 +1,21 @@
+package vjezba.pkg17;
+
+
+
+public class Vjezba17 {
+
+    public static void main(String[] args) {
+
+       NodeList list = new NodeList();
+       list.add(1);
+       list.add(5);
+       list.add(10);
+        System.out.println(list.getValue(2));
+
+        
+       
+        
+        
+    }
+
+}
